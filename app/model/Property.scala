9b6f82e914998a06cdd632b83c24ab0e88a19c4a
Karl Kegel <karl.kegel@flimey.io>
2020-07-21 21:05:11 +0200

    Fix Property doc

--- app/model/Property.scala
+++ app/model/Property.scala
@@ -18,4 +18,13 @@
 
 package model
 
+/**
+ * The Property data class.
+ * A Property can be used as AssetProperty or SubjectProperty.
+ *
+ * @param id unique identifier
+ * @param key of the property
+ * @param value of the property
+ * @param parentId id of the parent Asset/Subject
+ */
 case class Property(id: Long, key: String, value: String, parentId: Long)
