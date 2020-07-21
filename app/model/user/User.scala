88fc2ec68ff5538e204e2ab9877ec1c9de83701c
Karl Kegel <karl.kegel@flimey.io>
2020-07-21 21:06:14 +0200

    Implement User and Group model + db boilerplate

---
+++ app/model/user/User.scala
@@ -0,0 +1,34 @@
+/*
+ * This file is part of the flimey-core software.
+ * Copyright (C) 2020  Karl Kegel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * */
+
+package model.user
+
+/**
+ * The User data class.
+ *
+ * @param id unique identifier
+ * @param username visible name of the user
+ * @param email unique email address
+ * @param password password (hashed in db)
+ * @param role access role
+ * @param key pending authentication key
+ * @param accepted flag if user has accepted the usage conditions
+ * @param enabled flag if the user is allowed to log in
+ */
+case class User (id: Long, username: String, email: String, password: String, role: String,
+                 key: String, accepted: Boolean, enabled: Boolean)
--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,36 +1,74 @@
 -- !Ups
 
 create table `asset_type` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `value` VARCHAR(255) NOT NULL UNIQUE,
-  `active` BOOL NOT NULL
+    `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `value` VARCHAR(255) NOT NULL UNIQUE,
+    `active` BOOL NOT NULL
 );
 
 create table `asset_constraint` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `c` VARCHAR(255) NOT NULL,
-  `v1` VARCHAR(255) NOT NULL,
-  `v2` VARCHAR(255) NOT NULL,
-  `type_id` BIGINT NOT NULL,
-  FOREIGN KEY(type_id) REFERENCES asset_type(id)
+    `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `c` VARCHAR(255) NOT NULL,
+    `v1` VARCHAR(255) NOT NULL,
+    `v2` VARCHAR(255) NOT NULL,
+    `type_id` BIGINT NOT NULL,
+    FOREIGN KEY(type_id) REFERENCES asset_type(id)
 );
 
 create table `asset` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `type_id` BIGINT NOT NULL,
-  FOREIGN KEY(type_id) REFERENCES asset_type(id)
+    `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `type_id` BIGINT NOT NULL,
+    FOREIGN KEY(type_id) REFERENCES asset_type(id)
 );
 
 create table `asset_property` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `key` VARCHAR(255) NOT NULL,
-  `value` VARCHAR(255) NOT NULL,
-  `parent_id` BIGINT NOT NULL,
-  FOREIGN KEY(parent_id) REFERENCES asset(id)
+    `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `key` VARCHAR(255) NOT NULL,
+    `value` VARCHAR(255) NOT NULL,
+    `parent_id` BIGINT NOT NULL,
+    FOREIGN KEY(parent_id) REFERENCES asset(id)
 );
+
+create table `user` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `username` VARCHAR(255) NOT NULL UNIQUE,
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `role` VARCHAR(255) NOT NULL,
+    `key` VARCHAR(255),
+    `accepted` BOOL NOT NULL,
+    `enabled` BOOL NOT NULL
+);
+
+create table `u_group` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(255) NOT NULL UNIQUE
+);
+
+create table `asset_viewer` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `asset_id` BIGINT NOT NULL,
+    `group_id` BIGINT NOT NULL,
+    FOREIGN KEY(asset_id) REFERENCES asset(id),
+    FOREIGN KEY(group_id) REFERENCES u_group(id)
+);
+
+create table `group_membership` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `group_id` BIGINT NOT NULL,
+    `user_id` BIGINT NOT NULL,
+    FOREIGN KEY(group_id) REFERENCES u_group(id),
+    FOREIGN KEY(user_id) REFERENCES user(id)
+);
+
+INSERT INTO u_group(id, name) VALUES(0, 'public');
 
 -- !Downs
 
+drop table `group_membership`;
+drop table `asset_viewer`;
+drop table `u_group`;
+drop table `user`;
 drop table `asset_property`;
 drop table `asset_constraint`;
 drop table `asset`;
