5e56805dc70008330bb1a599b5c814e2a32b6388
Karl Kegel <karl.kegel@flimey.io>
2020-08-02 17:46:39 +0200

    Add auth to db conf and routes

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,3 +1,21 @@
+/*
+ * This file is part of the flimey-core software.
+ * Copyright (C) 2020  Karl Kegel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * */
+
 -- !Ups
 
 create table `asset_type` (
@@ -35,7 +53,7 @@ create table `user` (
     `email` VARCHAR(255) NOT NULL,
     `password` VARCHAR(255) NOT NULL,
     `role` VARCHAR(255) NOT NULL,
-    `key` VARCHAR(255),
+    `auth_key` VARCHAR(255),
     `accepted` BOOL NOT NULL,
     `enabled` BOOL NOT NULL
 );
@@ -61,7 +79,11 @@ create table `group_membership` (
     FOREIGN KEY(user_id) REFERENCES user(id)
 );
 
-INSERT INTO u_group(id, name) VALUES(0, 'public');
+-- Insert some demo and debug data here
+-- Remove those inserts in production!
+INSERT INTO u_group(id, name) VALUES(1, 'public');
+INSERT INTO user(id, username, email, password, role, auth_key, accepted, enabled) VALUES(1, 'Adam', 'admin@flimey.io', 'foo', 'admin', '', true, true);
+INSERT INTO group_membership(id, group_id, user_id) VALUES(0, 1, 1);
 
 -- !Downs
 
---
+++ conf/evolutions/default/2.sql
@@ -0,0 +1,41 @@
+/*
+ * This file is part of the flimey-core software.
+ * Copyright (C) 2020  Karl Kegel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * */
+
+-- Ups
+
+create table `auth_session` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `session` VARCHAR(255) NOT NULL,
+    `role` VARCHAR(255) NOT NULL,
+    `status` BOOL NOT NULL,
+    `user_id` BIGINT NOT NULL,
+    `created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+create table `access` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `session_id` BIGINT NOT NULL,
+    `group_id` BIGINT NOT NULL,
+    `group_name` VARCHAR(255) NOT NULL,
+    FOREIGN KEY(session_id) REFERENCES auth_session(id)
+);
+
+-- Downs
+
+DROP TABLE `access`;
+DROP TABLE `auth_session`;
