140b8929b105614ba25d0d61ff18f40a0354fd3a
Karl Kegel <karl.kegel@flimey.io>
2020-11-02 20:53:10 +0100

    Format user.scala

--- app/model/user/User.scala
+++ app/model/user/User.scala
@@ -38,8 +38,9 @@ case class User (id: Long, username: String, email: Option[String], password: Op
 object User {
 
   def applyRaw (id: Long, username: String, email: Option[String], password: Option[String], role: String,
-                key: Option[String], accepted: Boolean, enabled: Boolean): User =
+                key: Option[String], accepted: Boolean, enabled: Boolean): User = {
     User(id, username, email, password, Role.withName(role), key, accepted, enabled)
+  }
 
   def unapplyToRaw(arg: User): Option[(Long, String, Option[String], Option[String], String, Option[String], Boolean, Boolean)] =
     Option((arg.id, arg.username, arg.email, arg.password, arg.role.toString, arg.key, arg.accepted, arg.enabled))
