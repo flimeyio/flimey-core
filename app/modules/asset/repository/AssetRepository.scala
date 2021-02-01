b67ad57f102dd22d91eec24c249641810904ecf6
Karl Kegel <karl.kegel@flimey.io>
2021-02-01 19:52:27 +0100

    Refactor getSubset in AssetRepository more perfomant :zap:

--- app/modules/asset/repository/AssetRepository.scala
+++ app/modules/asset/repository/AssetRepository.scala
@@ -166,22 +166,31 @@ class AssetRepository @Inject()(@NamedDatabase("flimey_data") protected val dbCo
       (c, s) <- assets.filter(_.typeId === typeId) join
         viewers.filter(_.viewerId.inSet(groupIds)) on (_.entityId === _.targetId)
     } yield (c, s)).groupBy(_._1.id).map(_._1)
-    //FIXME basically, the limit should be applied here and not in the main query...
-    // but MYSQL is not able to support that and throws a runtime error. Maybe try with Postgres again.
+
+    val accessableAssets = assets.filter(_.id in subQuery).sortBy(_.id.desc).drop(offset).take(limit)
 
     //main query to fetch all data from the by the sub-query specified assets
-    db.run((for {
-      (c, s) <- ((assets.filter(_.id in subQuery).sortBy(_.id.desc).drop(offset).take(limit) join
-        properties on (_.entityId === _.parentId)) join
-        viewers on (_._1.entityId === _.targetId)) join
-        groups on (_._2.viewerId === _.id)
-    } yield (c, s)).result).map(res => {
-      val assets = res.groupBy(_._1._1._1)
-      assets.keys.map(asset => {
-        val parameters = assets(asset)
-        extendedAssetFromRaw(asset, parameters)
-      }).toSeq.sortBy(-_.asset.id)
-    })
+    val propertyQuery = for {
+      c <- accessableAssets join properties on (_.entityId === _.parentId)
+    } yield c
+
+    val viewerQuery = for {
+      c <- accessableAssets join (groups join viewers on (_.id === _.viewerId)) on (_.entityId === _._2.targetId)
+    } yield c
+
+    for {
+      propertyResult <- db.run(propertyQuery.result)
+      viewerResult <- db.run(viewerQuery.result)
+    } yield {
+        val assetsWithProperties = propertyResult.groupBy(_._1).mapValues(values => values.map(_._2))
+        val assetsWithViewers = viewerResult.groupBy(_._1).mapValues(values => values.map(_._2))
+
+        assetsWithProperties.keys.map(asset => {
+          val properties = assetsWithProperties(asset)
+          val viewerRelations = assetsWithViewers(asset)
+          ExtendedAsset(asset, properties, ViewerCombinator.fromRelations(viewerRelations))
+        }) toSeq
+      }
   }
 
   /**
