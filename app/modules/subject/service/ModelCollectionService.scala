2d37abfd475b72e3407844842e111a28c4987563
Karl Kegel <karl.kegel@flimey.io>
2021-02-01 22:41:33 +0100

    Update scaladoc in ModelCollectionService :book:

--- app/modules/subject/service/ModelCollectionService.scala
+++ app/modules/subject/service/ModelCollectionService.scala
@@ -31,12 +31,14 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 /**
- * FIXME
+ * The service class to provide safe functionality to work with [[modules.core.model.EntityType EntityTypes]] of
+ * [[modules.subject.model.Collection Collections]].
+ * <p> Normally, this class is used with dependency injection in controller classes or as helper in other services.
  *
- * @param typeRepository
- * @param constraintRepository
- * @param collectionRepository
- * @param entityTypeService
+ * @param typeRepository injected [[modules.core.repository.TypeRepository TypeRepository]]
+ * @param constraintRepository injected [[modules.core.repository.ConstraintRepository ConstraintRepository]]
+ * @param collectionRepository injected [[modules.subject.repository.CollectionRepository]]
+ * @param entityTypeService injected [[modules.core.service.EntityTypeService EntityTypeService]]
  */
 class ModelCollectionService @Inject()(typeRepository: TypeRepository,
                                        constraintRepository: ConstraintRepository,
