a6a071bbdcf2a058899d3c18160799946247204d
Karl Kegel <karl.kegel@flimey.io>
2021-02-04 20:41:46 +0100

    Create CollectibleLogic and CollectibleConstraintProcessor

--- app/modules/subject/service/CollectibleConstraintProcessor.scala
+++ app/modules/subject/service/CollectibleConstraintProcessor.scala
@@ -1,6 +1,6 @@
 /*
  * This file is part of the flimey-core software.
- * Copyright (C) 2020-2021 Karl Kegel
+ * Copyright (C) 2021 Karl Kegel
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,30 +18,49 @@
 
 package modules.subject.service
 
-import modules.core.util.ConstraintProcessor
 import modules.core.model.{Constraint, ConstraintType}
-import modules.subject.model.CollectibleConstraintSpec
-import modules.util.messages.{ERR, Status}
+import modules.core.util.ConstraintProcessor
+import modules.util.messages.{ERR, OK, Status}
 
 /**
  * Trait which provides functionality for parsing and processing constraints
  */
 trait CollectibleConstraintProcessor extends ConstraintProcessor {
-  
+
   /**
-   * Checks if a given Constraint is a syntactically correct Constraint of an AssetType.
+   * Checks if a given [[modules.core.model.Constraint Constraint]] is a syntactically correct Constraint of an
+   * [[modules.subject.model.Collectible Collectible]] [[modules.core.model.EntityType EntityType]]
    * No semantic analysis is done!
    *
    * @param constraint to check
    * @return Status with optional error message
    */
-    //FIXME adjust for collectible
   override def isValidConstraint(constraint: Constraint): Status = {
     constraint.c match {
-      case ConstraintType.HasProperty => isHasPropertyConstraint(constraint.v1, constraint.v2, CollectibleConstraintSpec.hasPropertyTypes)
       case ConstraintType.MustBeDefined => isMustBeDefinedConstraint(constraint.v1, constraint.v2)
+      case ConstraintType.UsesPlugin => isUsesPluginConstraint(constraint.v1, constraint.v2)
+      case ConstraintType.CanContain => isCanContainConstraint(constraint.v1, constraint.v2)
       case _ => ERR("Invalid Asset Constraint Rule")
     }
+  }
+
+  /**
+   * Checks if a [[modules.core.model.Constraint Constraint]] model is valid to model a [[modules.subject.model.Collectible Collectible]].
+   * <p> 1. Checks if HasProperty(s) and MustBeDefined(s) are matching
+   * <p> 2. Checks if no duplicate Constraints are present
+   * <p> 3. Checks if the configuration serves its UsesPlugin(s)
+   * <p> 4. Checks NOT for invalid Constraints without effect, use [[isValidConstraint]]
+   *
+   * @param constraints model to check
+   * @return Status with optional error message
+   */
+  override def isConstraintModel(constraints: Seq[Constraint]): Status = {
+    //check HasProperty, MustBeDefined Constraints and duplicates.
+    val checkBaseModelStatus = super.isConstraintModel(constraints)
+
+    if (!checkBaseModelStatus.valid) return checkBaseModelStatus
+    if (!hasCompletePlugins(constraints)) return ERR("Model requires properties to serve its plugins")
+    OK()
   }
 
 }
